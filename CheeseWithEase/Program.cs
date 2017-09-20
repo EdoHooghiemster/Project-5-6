860e6972363c2111d705105d1ba30122cbc0cccc
mikekeehnen <mikekeehnen@me.com>
2017-09-20 14:49:05 +0200

    YOLO

--- CheeseWithEase/Program.cs
+++ CheeseWithEase/Program.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using CheeseWithEase.Models;
+using System.Linq;
 
 namespace CheeseWithEase
 {
