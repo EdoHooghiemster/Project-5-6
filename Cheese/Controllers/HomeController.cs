a0f4ba24b7c9467e7ccf52eedb51629c6c958b8d
Edo Hooghiemster <0931317@hr.nl>
2017-10-06 10:58:05 +0200

    nav bar

--- Cheese/Controllers/HomeController.cs
+++ Cheese/Controllers/HomeController.cs
@@ -17,70 +17,55 @@ namespace Cheese.Controllers
 
         public IActionResult Land()
         {
-         
-
             return View();
         }
 
         public IActionResult Type()
         {
-
-
             return View();
         }
         public IActionResult Aanbiedingen()
         {
-     
-
             return View();
         }
 
         public IActionResult Kaasfondue()
         {
-            
-
             return View();
         }
 
         public IActionResult Kaasgereedschap()
         {
-            
-
             return View();
         }
 
         public IActionResult Lekker_bij_de_kaas()
         {
-            
-
             return View();
         }
 
          public IActionResult Kaaspakketten()
         {
-            
-
             return View();
         }
          public IActionResult About()
         {
-            
-
             return View();
         }
 
         public IActionResult Klantservice()
         {
-            
-
             return View();
         }
          public IActionResult Contact_Informatie()
         {
-            
-
             return View();
         }
+        public IActionResult Login()
+        {
+            return View();
+        }
+
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
