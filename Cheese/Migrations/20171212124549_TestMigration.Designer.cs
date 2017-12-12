03cfbc75f01c3c269afdde1577b58f2be5cdb188
Unknown <mikekeehnen@me.com>
2017-12-12 15:37:18 +0100

    webconfig changes

--- Cheese/Migrations/20171212124549_TestMigration.Designer.cs
+++ Cheese/Migrations/20171212124549_TestMigration.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace Cheese.Migrations
 {
     [DbContext(typeof(CheeseContext))]
-    partial class CheeseContextModelSnapshot : ModelSnapshot
+    [Migration("20171212124549_TestMigration")]
+    partial class TestMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
